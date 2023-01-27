c2c2b54828740567aeef8501426133cd2c9dc4d2
riavalon <raven.avalon5@gmail.com>
2023-01-27 12:26:04 -0330

    Fix asset path bug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const nextra = require('nextra')
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
-const PrPreviewPath = process.env.PR_PATH || ''
+const PrPreviewPath = process.env.PR_URI || ''
 
 let assetPrefix = '/'
 let basePath = ''
