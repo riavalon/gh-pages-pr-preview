44bddd11a918a780f9c3e5ddcbc2e7571c6b67a0
riavalon <raven.avalon5@gmail.com>
2023-01-27 11:23:12 -0330

    init

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const nextra = require('nextra')
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = '/'
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${ repo }/`
+  basePath = `/${ repo }`
+}
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+})
+
+module.exports = withNextra({
+  assetPrefix,
+  basePath,
+  images: {
+    unoptimized: true,
+  },
+})
---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+const repoUrl = 'https://github.com/riavalon/gh-pages-pr-preview'
+
+export default {
+  logo: <strong>Raven's PR Preview Test</strong>,
+  project: {
+    link: repoUrl,
+  },
+
+  docsRepositoryBase: repoUrl,
+}
